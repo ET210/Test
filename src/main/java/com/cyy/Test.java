e4456c96e006efc080212b1528d66f1ad2f93d4e
Tom <2102584124@qq.com>
2022-11-01 10:56:29 +0800

    first commmit

---
+++ src/main/java/com/cyy/Test.java
@@ -0,0 +1,14 @@
+package com.cyy;
+
+/**
+ * @program: Test
+ * @description:
+ * @author: 曹洋洋
+ * @create: 2022-11-01 10:54
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
